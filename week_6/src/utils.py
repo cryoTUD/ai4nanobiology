34518e127d827dd7cc36438f7bf136db5e6dc4c8
abharadwaj1 <a.bharadwaj@tudelft.nl>
2026-04-08 16:44:42 +0200

    access token

--- week_6/src/utils.py
+++ week_6/src/utils.py
@@ -2,10 +2,11 @@ def setup_client():
     import os 
     import json 
     from huggingface_hub import InferenceClient
-    
-    ACCESS_TOKEN_PATH="/Users/alokbharadwaj/Desktop/hf_access.json"
-    with open(ACCESS_TOKEN_PATH, "r") as f:
-        access_token = json.load(f)["token"]
+
+    # ACCESS_TOKEN_PATH="/Users/alokbharadwaj/Desktop/hf_access.json"
+    # with open(ACCESS_TOKEN_PATH, "r") as f:
+    #     access_token = json.load(f)["token"]
+    access_token = "hf_xxxxxxxxxxxxxx"
 
     client = InferenceClient(token=access_token)
     return client
